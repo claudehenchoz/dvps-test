f9e85f46a953f190e3e01aba24c730ee8a8d8423
Claude Henchoz <claude.henchoz@gmail.com>
2016-01-24 20:58:35 +0100

    Added one more test

--- Tests/dvps-test.Tests.ps1
+++ Tests/dvps-test.Tests.ps1
@@ -1,13 +1,16 @@
 Import-Module -Force $PSScriptRoot\..\dvps-test.ps1
 
 Describe 'Remove-StringLatinCharacters' {
-    
+
     Context 'Function' { 
 
-        Set-StrictMode -Version latest
-        
         It 'Should return Francois from Wush Washington' {
-            Remove-StringLatinCharacters 'François from Wüsh Washington' | Should BeExactly 'Francois from Wush Washington'
+            Remove-StringLatinCharacters 'François from Wüsh Washington' | `
+                Should BeExactly 'Francois from Wush Washington'
+        }
+
+        It 'Should return Francois from FFFF' {
+            Remove-StringLatinCharacters 'FFFF' | Should BeExactly 'FFFF'
         }
 
     }
