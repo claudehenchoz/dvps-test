19da5d981b733266651273fe2a1115979ec9eeae
Claude Henchoz <claude.henchoz@gmail.com>
2016-01-25 18:47:08 +0100

    test

--- Tests/dvps-test.Tests.ps1
+++ Tests/dvps-test.Tests.ps1
@@ -21,12 +21,20 @@ Describe 'Format-FileSize' {
 
     Context 'Function' { 
 
-        It '1048576 should return 1024.0 kB' {
+        It 'Should return 1024.0 kB for 1048576' {
             Format-FileSize 1048576 | Should Be '1024.0 kB'
         }
 
-        It '4724464025.6 should return 4.4 GB' {
+        It 'Should return 4.4 GB for 4724464025.6' {
             Format-FileSize 4724464025.6 | Should Be '4.4 GB'
+        }
+
+        It 'Should return 0.0 B for 0' {
+            Format-FileSize 0 | Should Be '0.0 B'
+        }
+
+        It 'Should throw on negative values' {
+            { Format-FileSize -2 } | Should throw
         }
 
     }
--- dvps-test.ps1
+++ dvps-test.ps1
@@ -7,11 +7,10 @@ function Remove-StringLatinCharacters {
 
 function Format-FileSize {
     Param([Parameter(Mandatory=$true)][long]$Size)
-    if (!$Size) { throw 'No size specified' }
     If     ($Size -gt 1TB) {[string]::Format("{0:0.0} TB", $Size / 1TB)}
     ElseIf ($Size -gt 1GB) {[string]::Format("{0:0.0} GB", $Size / 1GB)}
     ElseIf ($Size -gt 1MB) {[string]::Format("{0:0.0} MB", $Size / 1MB)}
     ElseIf ($Size -gt 1KB) {[string]::Format("{0:0.0} kB", $Size / 1KB)}
-    ElseIf ($Size -gt 0)   {[string]::Format("{0:0.0} B", $Size)}
-    Else                   {""}
+    ElseIf ($Size -ge 0)   {[string]::Format("{0:0.0} B", $Size)}
+    Else                   { throw }
 }
